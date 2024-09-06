31cb3c94f05daee7f9ad6a46e1aa7dad14e1cc97
jaakdentrekhaak <jens.borlo@hotmail.be>
2024-09-06 13:40:05 +0200

    remove build from repo

--- build/controllers/users/handlers/create.handler.js
+++
@@ -1,7 +0,0 @@
-import { UserStore } from "./user.store.js";
-export const create = async (body)=>{
-    const user = UserStore.add(body);
-    return user;
-};
-
-//# sourceMappingURL=create.handler.js.map
--- build/controllers/users/handlers/delete.handler.js
+++
@@ -1,6 +0,0 @@
-import { UserStore } from "./user.store.js";
-export const deleteUser = (id)=>{
-    UserStore.delete(Number(id));
-};
-
-//# sourceMappingURL=delete.handler.js.map
